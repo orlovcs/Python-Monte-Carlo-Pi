5f8340bc075af0c303e865307531398ba475f4a3
Nikita <thetax45@gmail.com>
2017-09-24 23:01:55 -0400

    Create monte.py

---
+++ monte.py
@@ -0,0 +1,15 @@
+def monte():
+  square_size = input("What should the size of the square be?: ")
+  left_bottom = [0,0]
+  left_top = [0,square_size]
+  right_bottom = [square_size,0]
+  right_top = [square_size,square_size]
+ 
+ 
+ print(left_top(0),left_top(1)"------",right_top(0),right_top(1))
+  
+  
+  for i in range(0,6):
+    print("|                        |")
+
+  print(left_bottom,"------",right_bottom)
